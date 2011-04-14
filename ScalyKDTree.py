ab7fb2565b2224dc9b7a9147423185db129226d0
Horia Coman <horia141@gmail.com>
2011-04-14 17:12:47 +0300

    Added actual tests.
    * I've added tests for KDNode and KDTree.

--- ScalyKDTree.py
+++ ScalyKDTree.py
@@ -1,11 +1,10 @@
-POINT_SIZE = 2
+import math
 
 class KDNode(object):
     def __init__(self,point,disc):
         assert isinstance(point,tuple)
         assert isinstance(disc,int)
-        assert len(point) == POINT_SIZE
-        assert disc >= 0 and disc < POINT_SIZE
+        assert disc >= 0 and disc < len(point)
 
         self.point = point
         self.disc = disc
@@ -18,9 +17,34 @@ class KDNode(object):
     def __str__(self):
         return '(' + str(self.disc) + '|' + ','.join(map(str,self.point)) + ')'
 
+    def __len__(self):
+        return len(self.point)
+
+    def __getitem__(self,key):
+        assert key >= 0 and key < len(self.point)
+
+        return self.point[key]
+
+    def equal_with_mask(self,point,mask):
+        assert isinstance(point,KDNode) or isinstance(point,tuple)
+        assert isinstance(mask,tuple)
+        assert len(self.point) == len(point)
+        assert len(point) == len(mask)
+        assert all(map(lambda x: isinstance(x,bool),mask))
+
+        for i in range(0,len(point)):
+            if mask[i] and self.point[i] != point[i]:
+                return False
+
+        return True
+
 class KDTree(object):
-    def __init__(self):
+    def __init__(self,size):
+        assert isinstance(size,int)
+        assert size > 1
+
         self.root = None
+        self.size = size
 
     @staticmethod
     def _node_to_str(node,level,mode):
@@ -35,28 +59,152 @@ class KDTree(object):
             return ''
         
     @staticmethod
-    def _insert(node,point):
-        if point[node.disc] < node.point[node.disc]:
+    def _insert(size,node,point):
+        if point[node.disc] <= node.point[node.disc]:
             if node.left == None:
-                node.left = KDNode(point,(node.disc + 1) % POINT_SIZE)
+                node.left = KDNode(point,(node.disc + 1) % size)
             else:
-                KDTree._insert(node.left,point)
+                KDTree._insert(size,node.left,point)
         else:
             if node.right == None:
-                node.right = KDNode(point,(node.disc + 1) % POINT_SIZE)
+                node.right = KDNode(point,(node.disc + 1) % size)
             else:
-                KDTree._insert(node.right,point)
+                KDTree._insert(size,node.right,point)
+
+    @staticmethod
+    def _find_exact(size,node,point):
+        if node == None:
+            return None
+        elif node.point == point:
+            return node
+        else:
+            if point[node.disc] <= node.point[node.disc]:
+                return KDTree._find_exact(size,node.left,point)
+            else:
+                return KDTree._find_exact(size,node.right,point)
+
+    @staticmethod
+    def _find_with_mask(size,node,point,mask,result):
+        if node == None:
+            return result
+        else:
+            if node.equal_with_mask(point,mask):
+                result.append(node)
+
+            if mask[node.disc]:
+                if point[node.disc] < node.point[node.disc]:
+                    KDTree._find_with_mask(size,node.left,point,mask,result)
+                elif point[node.disc] > node.point[node.disc]:
+                    KDTree._find_with_mask(size,node.right,point,mask,result)
+                else:
+                    KDTree._find_with_mask(size,node.left,point,mask,result)
+                    KDTree._find_with_mask(size,node.right,point,mask,result)
+            else:
+                KDTree._find_with_mask(size,node.left,point,mask,result)
+                KDTree._find_with_mask(size,node.right,point,mask,result)
+
+            return result
+
+    @staticmethod
+    def _node_point_distance(node,point):
+        return math.sqrt(sum(map(lambda (x,y): (x - y)*(x - y),zip(node.point,point))))
+
+    @staticmethod
+    def _find_nearest(size,node,point,debug_path):
+        if node == None:
+            return (None,float("inf"))
+        else:
+            if debug_path:
+                print node
+
+            if node.equal_with_mask(point,(True,) * size):
+                return (node,0)
+            elif point[node.disc] < node.point[node.disc]:
+                (best,distance) = KDTree._find_nearest(size,node.left,point,debug_path)
+                direction = 0
+            else:
+                (best,distance) = KDTree._find_nearest(size,node.right,point,debug_path)
+                direction = 1
+
+            if KDTree._node_point_distance(node,point) < distance:
+                (best,distance) = (node,KDTree._node_point_distance(node,point))
+
+            if math.fabs(node.point[node.disc] - point[node.disc]) < distance:
+                if direction == 0:
+                    (alt_best,alt_distance) = KDTree._find_nearest(size,node.right,point,debug_path)
+                else:
+                    (alt_best,alt_distance) = KDTree._find_nearest(size,node.left,point,debug_path)
+
+                if alt_distance < distance:
+                    return (alt_best,alt_distance)
+                else:
+                    return (best,distance)
+            else:
+                return (best,distance)
 
     def __str__(self):
         return KDTree._node_to_str(self.root,0,'r')
 
     def insert(self,point):
         assert isinstance(point,tuple)
-        assert len(point) == POINT_SIZE
+        assert len(point) == self.size
 
         if self.root == None:
             self.root = KDNode(point,0)
         else:
-            KDTree._insert(self.root,point)
+            KDTree._insert(self.size,self.root,point)
 
         return self
+
+    def find_exact(self,point):
+        assert isinstance(point,tuple)
+        assert len(point) == self.size
+
+        return KDTree._find_exact(self.size,self.root,point)
+
+    def find_with_mask(self,point,mask):
+        assert isinstance(point,tuple)
+        assert isinstance(mask,tuple)
+        assert len(point) == self.size
+        assert len(mask) == self.size
+        assert all(map(lambda x: isinstance(x,bool),mask))
+
+        return KDTree._find_with_mask(self.size,self.root,point,mask,[])
+
+    def find_nearest(self,point,debug_path=False):
+        assert isinstance(point,tuple)
+        assert isinstance(debug_path,bool)
+        assert len(point) == self.size
+
+        return KDTree._find_nearest(self.size,self.root,point,debug_path)[0]
+
+def make_for_test1():
+    q = KDTree()
+
+    q.insert((1,2))
+    q.insert((5,3))
+    q.insert((0.5,5))
+    q.insert((0.5,6))
+    q.insert((0.4,3))
+    q.insert((4,4))
+    q.insert((7,7))
+    q.insert((6,6))
+
+    return q
+
+def make_for_test2():
+    q = KDTree()
+
+    q.insert((5,5))
+    q.insert((2,3))
+    q.insert((2,4))
+    q.insert((3,6))
+    q.insert((8,2))
+    q.insert((6,7))
+    q.insert((8,4))
+    q.insert((8,1))
+    q.insert((6,2))
+    q.insert((9,2))
+    q.insert((4.95,0.25))
+
+    return q
--- ScalyKDTreeTest.py
+++ ScalyKDTreeTest.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import unittest
 
 from ScalyKDTree import KDNode
@@ -7,8 +5,233 @@ from ScalyKDTree import KDTree
 
 class KDNodeTest(unittest.TestCase):
     'Test harness for "KDNode".'
-    pass
+
+    def test_ctor(self):
+        'Constructor.'
+
+        a = KDNode((1,2),0)
+
+        self.assertEqual(a.point[0],1)
+        self.assertEqual(a.point[1],2)
+        self.assertEqual(a.disc,0)
+
+        b = KDNode((1,2,3,4),2)
+
+        self.assertEqual(b.point[0],1)
+        self.assertEqual(b.point[1],2)
+        self.assertEqual(b.point[2],3)
+        self.assertEqual(b.point[3],4)
+        self.assertEqual(b.disc,2)
+
+        c = KDNode((1,),0)
+
+        self.assertEqual(c.point[0],1)
+
+        self.assertRaises(AssertionError,KDNode,'hello',3)
+        self.assertRaises(AssertionError,KDNode,(1,2),'j')
+        self.assertRaises(AssertionError,KDNode,'hello','j')
+        self.assertRaises(AssertionError,KDNode,(1,2,3),4)
+
+    def test_repr(self):
+        'Python representation.'
+
+        a = KDNode((1,2,3),2)
+
+        self.assertEqual(repr(a),'KDNode((1, 2, 3),2)')
+
+    def test_str(self):
+        'String representation.'
+
+        a = KDNode((1,2,3),1)
+
+        self.assertEqual(str(a),'(1|1,2,3)')
+
+    def test_len(self):
+        'Obtaining the length.'
+
+        a = KDNode((1,2,3),1)
+
+        self.assertEqual(len(a),3)
+        
+        b = KDNode((1,),0)
+
+        self.assertEqual(len(b),1)
+
+        c = KDNode((1,2,4,10,20),4)
+
+        self.assertEqual(len(c),5)
+
+    def test_getitem(self):
+        'Array addressing.'
+
+        a = KDNode((1,2,3),1)
+        
+        self.assertEqual(a[0],1)
+        self.assertEqual(a[1],2)
+        self.assertEqual(a[2],3)
+
+        self.assertRaises(AssertionError,KDNode.__getitem__,a,4)
+        self.assertRaises(AssertionError,KDNode.__getitem__,a,-2)
+
+    def test_equal_with_mask(self):
+        'Masked equality.'
+
+        a = KDNode((1,3,2),1)
+        b = KDNode((1,3,2),1)
+        c = KDNode((1,4,2),2)
+
+        self.assertTrue(a.equal_with_mask(b,(True,True,True)))
+        self.assertFalse(a.equal_with_mask(c,(True,True,True)))
+        self.assertTrue(a.equal_with_mask(c,(True,False,True)))
+        self.assertTrue(a.equal_with_mask(b,(True,False,True)))
+        self.assertTrue(a.equal_with_mask(b,(False,False,False)))
+        self.assertTrue(a.equal_with_mask(c,(False,False,False)))
+
+        self.assertTrue(a.equal_with_mask((1,3,2),(True,True,True)))
+        self.assertFalse(a.equal_with_mask((1,4,2),(True,True,True)))
+        self.assertTrue(a.equal_with_mask((1,4,2),(True,False,True)))
+        self.assertTrue(a.equal_with_mask((1,3,2),(True,False,True)))
+        self.assertTrue(a.equal_with_mask((1,3,2),(False,False,False)))
+        self.assertTrue(a.equal_with_mask((1,4,2),(False,False,False)))
+
+        self.assertRaises(AssertionError,KDNode.equal_with_mask,a,'ola',(True,True,True))
+        self.assertRaises(AssertionError,KDNode.equal_with_mask,a,(1,2,3),'time')
+        self.assertRaises(AssertionError,KDNode.equal_with_mask,a,(1,2,3,4),(True,False,True))
+        self.assertRaises(AssertionError,KDNode.equal_with_mask,a,(1,2,3),(True,False,True,False))
+        self.assertRaises(AssertionError,KDNode.equal_with_mask,a,(3,2,1),(1,0,0))
+        
 
 class KDTreeTest(unittest.TestCase):
     'Test harness for "KDTree".'
-    pass
+
+    def test_ctor(self):
+        'Constructor.'
+
+        a = KDTree(2)
+
+        self.assertEqual(a.root,None)
+        self.assertEqual(a.size,2)
+
+        self.assertRaises(AssertionError,KDTree,'t')
+        self.assertRaises(AssertionError,KDTree,0)
+
+    def test_insert(self):
+        'Insertion.'
+
+        a = KDTree(3)
+
+        a.insert((1,1,1))
+
+        self.assertTrue(a.root.equal_with_mask((1,1,1),(True,True,True)))
+
+        b = KDTree(2)
+
+        b.insert((5,5))
+        b.insert((2,3))
+        b.insert((2,4))
+        b.insert((3,6))
+        b.insert((8,2))
+        b.insert((6,7))
+        b.insert((8,4))
+        b.insert((8,1))
+        b.insert((6,2))
+        b.insert((9,2))
+        b.insert((4.95,0.25))
+
+        self.assertTrue(b.root.equal_with_mask((5,5),(True,True)))
+        self.assertTrue(b.root.left.equal_with_mask((2,3),(True,True)))
+        self.assertTrue(b.root.left.left.equal_with_mask((4.95,0.25),(True,True)))
+        self.assertTrue(b.root.left.right.equal_with_mask((2,4),(True,True)))
+        self.assertTrue(b.root.left.right.right.equal_with_mask((3,6),(True,True)))
+        self.assertTrue(b.root.right.equal_with_mask((8,2),(True,True)))
+        self.assertTrue(b.root.right.left.equal_with_mask((8,1),(True,True)))
+        self.assertTrue(b.root.right.left.left.equal_with_mask((6,2),(True,True)))
+        self.assertTrue(b.root.right.left.right.equal_with_mask((9,2),(True,True)))
+        self.assertTrue(b.root.right.right.equal_with_mask((6,7),(True,True)))
+        self.assertTrue(b.root.right.right.right.equal_with_mask((8,4),(True,True)))
+
+        c = KDTree(3)
+
+        self.assertRaises(AssertionError,KDTree.insert,c,'hello')
+        self.assertRaises(AssertionError,KDTree.insert,c,(1,2))
+
+    def test_find_exact(self):
+        'Find exact match.'
+
+        a = KDTree(2)
+
+        a.insert((5,5))
+        a.insert((2,3))
+        a.insert((2,4))
+        a.insert((3,6))
+        a.insert((8,2))
+        a.insert((6,7))
+        a.insert((8,4))
+        a.insert((8,1))
+        a.insert((6,2))
+        a.insert((9,2))
+        a.insert((4.95,0.25))
+
+        self.assertEqual(a.find_exact((5,5)).point,(5,5))
+        self.assertEqual(a.find_exact((2,3)).point,(2,3))
+        self.assertEqual(a.find_exact((5,8)),None)
+
+        self.assertRaises(AssertionError,KDTree.find_exact,a,'hello')
+        self.assertRaises(AssertionError,KDTree.find_exact,a,(1,2,3))
+
+    def test_find_with_mask(self):
+        'Find masked match.'
+
+        a = KDTree(2)
+
+        a.insert((5,5))
+        a.insert((2,3))
+        a.insert((2,4))
+        a.insert((3,6))
+        a.insert((8,2))
+        a.insert((6,7))
+        a.insert((8,4))
+        a.insert((8,1))
+        a.insert((6,2))
+        a.insert((9,2))
+        a.insert((4.95,0.25))
+
+        self.assertEqual(map(lambda x: x.point,a.find_with_mask((8,-1),(True,False))),[(8,2),(8,1),(8,4)])
+        self.assertEqual(map(lambda x: x.point,a.find_with_mask((6,-1),(True,False))),[(6,2),(6,7)])
+        self.assertEqual(map(lambda x: x.point,a.find_with_mask((8,1),(True,True))),[(8,1)])
+        self.assertEqual(map(lambda x: x.point,a.find_with_mask((1,2),(True,True))),[])
+
+        self.assertRaises(AssertionError,KDTree.find_with_mask,a,'helo',(True,False))
+        self.assertRaises(AssertionError,KDTree.find_with_mask,a,(1,2),'j')
+        self.assertRaises(AssertionError,KDTree.find_with_mask,a,'helo','j')
+        self.assertRaises(AssertionError,KDTree.find_with_mask,a,(1,2,3),(True,False))
+        self.assertRaises(AssertionError,KDTree.find_with_mask,a,(1,2),(True,True,False))
+        self.assertRaises(AssertionError,KDTree.find_with_mask,a,(1,2),(True,True,'true'))
+
+    def test_find_nearest(self):
+        'Find nearest match.'
+
+        a = KDTree(2)
+
+        a.insert((5,5))
+        a.insert((2,3))
+        a.insert((2,4))
+        a.insert((3,6))
+        a.insert((8,2))
+        a.insert((6,7))
+        a.insert((8,4))
+        a.insert((8,1))
+        a.insert((6,2))
+        a.insert((9,2))
+        a.insert((4.95,0.25))
+
+        self.assertEqual(a.find_nearest((7.5,2.5)).point,(8,2))
+        self.assertEqual(a.find_nearest((7.5,1.5)).point,(8,1))
+        self.assertEqual(a.find_nearest((5.01,0.22)).point,(4.95,0.25))
+        self.assertEqual(a.find_nearest((3,6)).point,(3,6))
+        self.assertEqual(a.find_nearest((5,5)).point,(5,5))
+        self.assertEqual(a.find_nearest((8,3)).point,(8,4))
+
+        self.assertRaises(AssertionError,KDTree.find_nearest,a,'hello')
+        self.assertRaises(AssertionError,KDTree.find_nearest,a,(1,2,3))
+        self.assertRaises(AssertionError,KDTree.find_nearest,a,(1,2),'yello')
